b35d753b89496f984580b2ef60101231c2c31871
Wanjin Maglangit <wmaglangit1118@gmail.com>
2023-02-15 17:37:54 +0800

    added checking of 9x9 box, valid function complete

--- sudoku.py
+++ sudoku.py
@@ -40,5 +40,12 @@ def valid(board, number, position):
             return False
 
     #Check box
-    
+    box_x = position[1] // 3
+    box_y = position[0] // 3
         
+    for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
